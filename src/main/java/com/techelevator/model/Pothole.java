cfa5cec4907ad797d140be77679f5a5277c3bb80
Mason Burton <burtonmasonlee@gmail.com>
2019-08-05 16:00:19 -0400

    Updated index with for each of potholes

--- src/main/java/com/techelevator/model/Pothole.java
+++ src/main/java/com/techelevator/model/Pothole.java
@@ -3,53 +3,68 @@ package com.techelevator.model;
 import java.time.LocalDate;
 
 public class Pothole {
-	
+
 	private long id;
 	private Address address;
 	private String size;
 	private String description;
-	private LocalDate createdAt;
+	private LocalDate createdOn;
 	private String longitude;
 	private String latitude;
+
+	// getters and setters
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+
 	public String getSize() {
 		return size;
 	}
+
 	public void setSize(String size) {
 		this.size = size;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public LocalDate getCreatedAt() {
-		return createdAt;
+
+	public LocalDate getCreatedOn() {
+		return createdOn;
 	}
-	public void setCreatedAt(LocalDate createdAt) {
-		this.createdAt = createdAt;
+
+	public void setCreatedOn(LocalDate createdOn) {
+		this.createdOn = createdOn;
 	}
+
 	public String getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
+
 	public String getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
