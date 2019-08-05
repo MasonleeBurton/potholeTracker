9438b1dc41e206c4cb12cc39a13950319c5f7ded
Isaac <isaac13smith@gmail.com>
2019-08-05 15:22:25 -0400

    Setup pothole controller and DAO

---
+++ src/main/java/com/techelevator/model/Address.java
@@ -0,0 +1,41 @@
+package com.techelevator.model;
+
+public class Address {
+
+	private long id;
+	private int zipCode;
+	private String city;
+	private String addressLine1;
+	private String addressLine2;
+	
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+}
