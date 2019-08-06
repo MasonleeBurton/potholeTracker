c4d9419c6c7acc25daf763692f2b91bb20f368e6
Scott <scottsikon@gmail.com>
2019-08-06 10:13:57 -0400

    Tue 10:13AM

--- database/data.sql
+++ database/data.sql
@@ -13,7 +13,7 @@ VALUES (default, 43229, 'Columbus', '123 Elm Street', 'Apt B' );
 
 SELECT * FROM pothole;
 INSERT INTO pothole(id, address_id, size, description, created_on, longitude, latitude )
-VALUES (default, 1, 'large', 'Its a doozie', 8/6/2019, 123456, 1234567 );
+VALUES (default, 1, 'large', 'Its a doozie', '8/6/2019', 123456, 1234567 );
 
 
 SELECT * FROM address;
@@ -22,7 +22,7 @@ VALUES (default, 43229, 'Columbus', '666 Elm Street', 'Apt A' );
 
 SELECT * FROM pothole;
 INSERT INTO pothole(id, address_id, size, description, created_on, longitude, latitude )
-VALUES (default, 2, 'medium', 'whoa doggy', 8/6/2019, 123457, 1234568 );
+VALUES (default, 2, 'medium', 'whoa doggy', '8/6/2019', 123457, 1234568 );
 
 
 SELECT * FROM address;
@@ -31,7 +31,7 @@ VALUES (default, 43229, 'Columbus', '1234 Street St' );
 
 SELECT * FROM pothole;
 INSERT INTO pothole(id, address_id, size, description, created_on, longitude, latitude )
-VALUES (default, 3, 'small', 'whoa nelly', 8/6/2019, 123458, 1234569 );
+VALUES (default, 3, 'small', 'whoa nelly', '8/6/2019', 123458, 1234569 );
 
 
 
