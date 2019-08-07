e605aec96314b85239d439a566cbbd491d7f54e8
Scott <scottsikon@gmail.com>
2019-08-07 15:39:15 -0400

    Wed 3:39PM

--- database/data.sql
+++ database/data.sql
@@ -8,7 +8,7 @@ BEGIN;
 
 SELECT * FROM address;
 INSERT INTO address (address_id, zip_code, city, address_line_1, address_line_2, state  )
-VALUES (1, 43229, 'Columbus', '123 Elm Street', 'Apt B', Ohio );
+VALUES (1, 43229, 'Columbus', '123 Elm Street', 'Apt B', 'Ohio' );
 
 
 SELECT * FROM pothole;
@@ -18,7 +18,7 @@ VALUES (default, 1, 'large', 'Its a doozie', '8/6/2019', 123456, 1234567, 1 );
 
 SELECT * FROM address;
 INSERT INTO address (address_id, zip_code, city, address_line_1, address_line_2, state  )
-VALUES (2, 43229, 'Columbus', '666 Elm Street', 'Apt A', Ohio );
+VALUES (2, 43229, 'Columbus', '666 Elm Street', 'Apt A', 'Ohio' );
 
 SELECT * FROM pothole;
 INSERT INTO pothole(id, address_id, size, description, created_on, longitude, latitude, status_id )
@@ -27,7 +27,7 @@ VALUES (default, 2, 'medium', 'whoa doggy', '8/6/2019', 123457, 1234568, 2 );
 
 SELECT * FROM address;
 INSERT INTO address (address_id, zip_code, city, address_line_1, address_line_2, state  )
-VALUES (3, 43229, 'Columbus', '1234 Street St', 'asd', Ohio );
+VALUES (3, 43229, 'Columbus', '1234 Street St', 'asd', 'Ohio' );
 
 SELECT * FROM pothole;
 INSERT INTO pothole(id, address_id, size, description, created_on, longitude, latitude, status_id )
