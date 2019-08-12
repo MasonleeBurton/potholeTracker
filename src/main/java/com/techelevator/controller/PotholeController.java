7babd10d6b2fb0ee6c43f1cfb9b18ab5b5298e98
Miranda <mirandarregan@gmail.com>
2019-08-12 14:23:14 -0400

    Images uploaded and shown

--- src/main/java/com/techelevator/controller/PotholeController.java
+++ src/main/java/com/techelevator/controller/PotholeController.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -17,8 +19,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.techelevator.model.Pothole;
@@ -51,7 +57,26 @@ public class PotholeController {
 		map.addAttribute("potholes", potholeDao.getAll());
 		map.addAttribute("status", new Status());
 
+		String imagePath = getServerContextPath() + File.separator;
+		
+		map.addAttribute("imagePath", imagePath);
+
 		return "index";
+	}
+	
+	@RequestMapping(path="/image/{imageName}", method=RequestMethod.GET)
+	@ResponseBody
+	public byte[] getImage(@PathVariable(value="imageName") String imageName) {
+		String imagePath = getServerContextPath() + File.separator + imageName;
+		File image = new File(imagePath);
+		try {
+			return Files.readAllBytes(image.toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 
 	@PostMapping("/update")
