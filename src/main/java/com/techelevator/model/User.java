04894fef1e06e53ec16d08993db9a51510cfe484
Miranda <mirandarregan@gmail.com>
2019-08-14 14:24:22 -0400

    Added user to pothole connection

--- src/main/java/com/techelevator/model/User.java
+++ src/main/java/com/techelevator/model/User.java
@@ -20,6 +20,14 @@ public class User {
 	private String role;
 	
 	private String confirmPassword;
+	private long userId;
+	
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
 	public String getUserName() {
 		return userName;
 	}
--- src/main/java/com/techelevator/model/dao/PotholeDAO.java
+++ src/main/java/com/techelevator/model/dao/PotholeDAO.java
@@ -9,12 +9,14 @@ public interface PotholeDAO {
 	
 	public List<Pothole> getAll();
 
-	public void create(Pothole pothole);
+	
 
 	public void updateStatus(Status status, long potholeId);
 
 	public void delete(long potholeId);
 
 	Pothole getPotholeById(long potholeId);
+
+	void create(Pothole pothole, long userId);
 
 }
